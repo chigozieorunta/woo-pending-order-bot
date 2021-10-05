07b86fbc5353750402c820e435fef5a09371926e
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-05 16:48:25 +0100

    Set up basic plugin file

---
+++ woo-pending-order-bot.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Pending Order Reminder Bot
+ * Description: Send SMS messages to buyers notifying them of abandoned cart orders.
+ * Version: 1.0.0
+ * Author: Chigozie Orunta
+ * Author URI: https://github.com/chigozieorunta/woo-pending-order-bot
+ * Text Domain: wporb
+ *
+ * @package WooPendingOrderBot
+ */
+
+\WooPendingOrderBot\Plugin::init();
