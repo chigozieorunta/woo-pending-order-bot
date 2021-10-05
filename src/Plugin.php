58b6d458175d566d9e8a0238c307b160b593789b
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-05 23:42:26 +0100

    Setup Plugin class as main base class

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin class.
+ *
+ * @package WooPendingOrderBot
+ */
+
+namespace WooPendingOrderBot;
+
+/**
+ * WordPress plugin interface.
+ */
+class Plugin {
+
+	/**
+	 * Setup the plugin.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		//..
+	}
+}
