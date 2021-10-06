f1b4aba20f57b94deacec7fb316c3848987a2cd1
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-06 05:54:34 +0100

    Established Singleton as single entry point

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,11 +13,31 @@ namespace WooPendingOrderBot;
 class Plugin {
 
 	/**
+	 * Plugin's singleton instance
+	 *
+	 * @var object
+	 */
+	private static $instance;
+
+	/**
 	 * Setup the plugin.
 	 *
 	 * @return void
 	 */
 	public function __construct() {
 		// ..
+	}
+
+	/**
+	 * Plugin Entry point based on Singleton
+	 *
+	 * @return Plugin $plugin Instance of the plugin abstraction.
+	 */
+	public static function init() {
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
 	}
 }
