c082e03324791ba4a3e44a63d9dec3a87275155c
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-06 06:17:58 +0100

    Set up Options class

---
+++ src/Options.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Options class.
+ *
+ * @package WooPendingOrderBot
+ */
+
+namespace WooPendingOrderBot;
+
+/**
+ * WordPress options page
+ */
+class Options {
+
+	/**
+	 * Instantiate class
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		// ..
+	}
+}
