a5386528fe37d216d4dd2bdaf7cc73909afd2430
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-12 00:11:18 +0100

    Defined private variable pending_orders

--- src/Order.php
+++ src/Order.php
@@ -13,11 +13,23 @@ namespace WooPendingOrderBot;
 class Order {
 
 	/**
+	 * Array of pending orders
+	 *
+	 * @var array
+	 */
+	private $pending_orders;
+
+	/**
 	 * Setup Order Class
 	 *
 	 * @return void
 	 */
 	public function __construct() {
-		// ..
+		$this->pending_orders = wc_get_orders(
+			array(
+				'limit'  => -1,
+				'status' => 'pending',
+			)
+		);
 	}
 }
