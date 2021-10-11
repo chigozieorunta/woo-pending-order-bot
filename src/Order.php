a01e991e83b0e14152a76531e26dc4c600e5c06d
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-12 00:16:07 +0100

    Check if WooCommerce exists and perform query

--- src/Order.php
+++ src/Order.php
@@ -25,11 +25,23 @@ class Order {
 	 * @return void
 	 */
 	public function __construct() {
-		$this->pending_orders = wc_get_orders(
-			array(
-				'limit'  => -1,
-				'status' => 'pending',
-			)
-		);
+		$this->pending_orders = [];
+		if ( ! class_exists( 'WooCommerce' ) ) {
+			$this->pending_orders = wc_get_orders(
+				array(
+					'limit'  => -1,
+					'status' => 'pending',
+				)
+			);
+		}
+	}
+
+	/**
+	 * Return pending orders
+	 *
+	 * @return array
+	 */
+	public function get_pending_orders() {
+		return $pending_orders;
 	}
 }
