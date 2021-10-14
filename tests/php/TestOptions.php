c46241be156e90adae7a5f600412b284a2cb73ac
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-14 23:23:51 +0100

    Setup test file for Options page

---
+++ tests/php/TestOptions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Tests for Options class.
+ *
+ * @package WooPendingOrderBot
+ */
+
+namespace WooPendingOrderBot;
+
+use WP_Mock;
+
+/**
+ * Test the Options class
+ */
+class TestOptions extends TestCase {
+
+}
