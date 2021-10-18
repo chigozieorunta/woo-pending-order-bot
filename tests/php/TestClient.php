851c20e18704e0b1260d72e6828f8ad678cf7a5c
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-18 23:48:54 +0100

    Added class for testing client interface

---
+++ tests/php/TestClient.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Tests for Client interface.
+ *
+ * @package WooPendingOrderBot
+ */
+
+namespace WooPendingOrderBot;
+
+use WP_Mock;
+
+/**
+ * Test the Client Interface
+ */
+class TestClient extends TestCase {
+
+}
