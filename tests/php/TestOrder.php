91ae3e7bb6241608fcfb06a9fff630d12d35470b
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-20 02:07:27 +0100

    Set up Order class testing

---
+++ tests/php/TestOrder.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Tests for Order class.
+ *
+ * @package WooPendingOrderBot
+ */
+
+namespace WooPendingOrderBot;
+
+use WP_Mock;
+
+/**
+ * Test the Order class
+ */
+class TestOrder extends TestCase {
+
+}
