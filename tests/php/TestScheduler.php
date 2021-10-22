a032ed6528f6a7e23b750dbdf572a34d89ee739d
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-22 12:50:35 +0100

    Added test class for Scheduler

---
+++ tests/php/TestScheduler.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Tests for Scheduler class.
+ *
+ * @package WooPendingOrderBot
+ */
+
+namespace WooPendingOrderBot;
+
+use WP_Mock;
+
+/**
+ * Test the Scheduler class
+ */
+class TestScheduler extends TestCase {
+
+}
