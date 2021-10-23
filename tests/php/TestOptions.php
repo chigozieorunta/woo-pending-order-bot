112ea3ec9c67b066d50f7f6b35bf1d9a376b1e8e
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-23 15:01:19 +0100

    Created a new test class for Options page

--- tests/php/TestOptions.php
+++ tests/php/TestOptions.php
@@ -7,11 +7,18 @@
 
 namespace WooPendingOrderBot;
 
+use Mockery;
 use WP_Mock;
 
 /**
  * Test the Options class
  */
 class TestOptions extends TestCase {
-
+	/**
+	 * Test get_sender.
+	 *
+	 * @covers \WooPendingOrderBot\Options::get_sender()
+	 */
+	public function test_get_sender() {
+	}
 }
---
+++ tests/php/TestTwilio.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Tests for Twilio class.
+ *
+ * @package WooPendingOrderBot
+ */
+
+namespace WooPendingOrderBot;
+
+use Mockery;
+use WP_Mock;
+
+/**
+ * Test the Twilio class
+ */
+class TestTwilio extends TestCase {
+
+}
